d8aaa03732991460b97d2728585b16c69f810512
Will Madden <w.a.madden@gmail.com>
2025-10-13 09:58:09 +0200

    Correct a bug in the tsup build config
    See https://github.com/egoist/tsup/issues/571
    
    - Use tsup to generate types
    - Delete tsbuildinfo artifacts
    - Correct bug

--- packages/relational-ir/tsup.config.ts
+++ packages/relational-ir/tsup.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['src/index.ts'],
+  entry: ['src/exports/index.ts', 'src/exports/schema.ts'],
   format: ['esm'],
-  dts: false,
+  dts: {
+    compilerOptions: {
+      composite: false,
+    },
+  },
   clean: true,
   sourcemap: true,
   external: ['@prisma/*'], // Don't bundle workspace deps
--- packages/sql/tsup.config.ts
+++ packages/sql/tsup.config.ts
@@ -3,7 +3,11 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: ['src/index.ts'],
   format: ['esm'],
-  dts: false,
+  dts: {
+    compilerOptions: {
+      composite: false,
+    },
+  },
   clean: true,
   sourcemap: true,
   external: ['@prisma/*'], // Don't bundle workspace deps
