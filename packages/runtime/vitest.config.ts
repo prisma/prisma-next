f84d61f2f2fcf3b5b57ea6df402c11dbd6d1e4f3
Will Madden <madden@prisma.io>
2025-11-09 12:57:44 +0100

    Extend coverage in runtime package

--- packages/runtime/vitest.config.ts
+++ packages/runtime/vitest.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
         '**/*.test-d.ts',
         '**/*.config.ts',
         '**/exports/**',
+        '**/plugins/types.ts', // Types-only file
       ],
       thresholds: {
         lines: 80,
