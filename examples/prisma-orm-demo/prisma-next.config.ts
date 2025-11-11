f1453da34eddce8a0cb4157feadd4de37fa4fa2f
Will Madden <madden@prisma.io>
2025-11-11 21:56:47 +0100

    Add missing deps

--- examples/prisma-orm-demo/prisma-next.config.ts
+++ examples/prisma-orm-demo/prisma-next.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from '@prisma-next/cli/config-types';
 import postgresAdapter from '@prisma-next/adapter-postgres/cli';
-import postgres from '@prisma-next/targets-postgres/cli';
+import { defineConfig } from '@prisma-next/cli/config-types';
 import sql from '@prisma-next/family-sql/cli';
+import postgres from '@prisma-next/targets-postgres/cli';
 
 export default defineConfig({
   family: sql,
