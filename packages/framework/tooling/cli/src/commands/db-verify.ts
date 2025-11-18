87713cc888261d9847951ce03ae95ba1c8d08557
Will Madden <madden@prisma.io>
2025-11-18 14:21:05 +0100

    Improve output

--- packages/framework/tooling/cli/src/commands/db-verify.ts
+++ packages/framework/tooling/cli/src/commands/db-verify.ts
@@ -156,7 +156,7 @@ export function createDbVerifyCommand(): Command {
                   driver,
                   contractIR,
                   expectedTargetId: config.target.targetId,
-                  contractPath,
+                  contractPath: contractPathAbsolute,
                   configPath,
                 }),
               {
