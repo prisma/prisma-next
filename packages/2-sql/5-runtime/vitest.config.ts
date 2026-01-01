3ff3d823c59807e2594b6d338a0527830a4688e6
Will Madden <madden@prisma.io>
2026-01-02 08:50:15 +1100

    Update sql-runtime TODO comments with Linear ticket TML-1786

--- packages/2-sql/5-runtime/vitest.config.ts
+++ packages/2-sql/5-runtime/vitest.config.ts
@@ -18,11 +18,11 @@ export default defineConfig({
         '**/*.test-d.ts',
         '**/*.config.ts',
         '**/exports/**',
-        'src/lower-sql-plan.ts', // TODO(TML-XXXX): Add tests - currently 0% coverage
-        'src/sql-marker.ts', // TODO(TML-XXXX): Add tests - currently 42% coverage
-        'src/codecs/encoding.ts', // TODO(TML-XXXX): Add tests - currently 6% coverage
-        'src/codecs/decoding.ts', // TODO(TML-XXXX): Add tests - currently 33% coverage
-        'src/codecs/validation.ts', // TODO(TML-XXXX): Add tests - currently 50% coverage
+        'src/lower-sql-plan.ts', // TODO(TML-1786): Add tests - currently 0% coverage
+        'src/sql-marker.ts', // TODO(TML-1786): Add tests - currently 42% coverage
+        'src/codecs/encoding.ts', // TODO(TML-1786): Add tests - currently 6% coverage
+        'src/codecs/decoding.ts', // TODO(TML-1786): Add tests - currently 33% coverage
+        'src/codecs/validation.ts', // TODO(TML-1786): Add tests - currently 50% coverage
       ],
       thresholds: {
         lines: 70,
