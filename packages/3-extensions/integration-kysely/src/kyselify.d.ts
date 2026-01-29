2c7f7bc929332330e9687c7f61df73423982b51c
Will Madden <madden@prisma.io>
2026-01-29 14:38:31 +0700

    refactor(integration-kysely): reuse TablesOf and ColumnsOf from relational-core
    Remove duplicate type definitions and import existing utilities from
    @prisma-next/sql-relational-core. This reduces code duplication and
    ensures the Kysely integration uses the same types as other SQL lanes.
    
    - Remove local TablesOf, ColumnsOf, GimmeStorageColumn definitions
    - Import TablesOf, ColumnsOf from sql-relational-core/types
    - Make KyselifyStorageColumn internal (not exported)

--- packages/3-extensions/integration-kysely/src/kyselify.d.ts
+++ packages/3-extensions/integration-kysely/src/kyselify.d.ts
@@ -1,34 +1,21 @@
+import type { CodecTypes, ColumnsOf, TablesOf } from '@prisma-next/sql-relational-core/types';
 import type { SqlContract, StorageColumn } from '@prisma-next/sql-contract/types';
 
 export type KyselifyContract<TContract extends SqlContract> = {
   -readonly [TableName in keyof TablesOf<TContract>]-?: {
-    -readonly [ColumnName in keyof ColumnsOf<TContract, TableName>]-?: KyselifyStorageColumn<
-      GimmeStorageColumn<TContract, TableName, ColumnName>,
-      TContract['mappings']['codecTypes'][GimmeStorageColumn<
+    -readonly [ColumnName in keyof ColumnsOf<TContract, TableName & string>]-?: KyselifyStorageColumn<
+      ColumnsOf<TContract, TableName & string>[ColumnName],
+      TContract['mappings']['codecTypes'][ColumnsOf<
         TContract,
-        TableName,
-        ColumnName
-      >['codecId']]
+        TableName & string
+      >[ColumnName]['codecId']]
     >;
   };
 };
 
-export type TablesOf<TContract extends SqlContract> = TContract['storage']['tables'];
-
-export type ColumnsOf<
-  TContract extends SqlContract,
-  TTableName extends keyof TablesOf<TContract>,
-> = TablesOf<TContract>[TTableName]['columns'];
-
-export type GimmeStorageColumn<
-  TContract extends SqlContract,
-  TTableName extends keyof TablesOf<TContract>,
-  TColumnName extends keyof ColumnsOf<TContract, TTableName>,
-> = ColumnsOf<TContract, TTableName>[TColumnName];
-
-export type KyselifyStorageColumn<
+type KyselifyStorageColumn<
   TColumn,
-  TCodec extends SqlContract['mappings']['codecTypes'][string],
+  TCodec extends CodecTypes[string],
 > = TColumn extends StorageColumn
   ? (TColumn['nullable'] extends true ? null : never) | TCodec['output']
   : never;
