87d5c686fe3e727f0c9d958e993db3c6e77e6606
Will Madden <madden@prisma.io>
2026-02-18 06:55:12 +0000

    chore(postgres): remove old targets path after move
    Finish moving @prisma-next/postgres by deleting the old package location under packages/3-targets/8-clients/postgres.

--- packages/3-targets/8-clients/postgres/src/exports/runtime.ts
+++
@@ -1,9 +0,0 @@
-export type { PostgresBinding } from '../runtime/binding';
-export type {
-  PostgresClient,
-  PostgresOptions,
-  PostgresOptionsBase,
-  PostgresOptionsWithContract,
-  PostgresOptionsWithContractJson,
-} from '../runtime/postgres';
-export { default } from '../runtime/postgres';
