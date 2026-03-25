f1cdac563ce0ec34b21692b2d36e25cf9b20988a
Will Madden <madden@prisma.io>
2026-03-25 14:44:46 +0100

    Delete lots of obsolete files and content

--- examples/prisma-orm-demo/src/queries/create-user.ts
+++
@@ -1,6 +0,0 @@
-import { getPrisma } from '../prisma/client';
-
-export async function createUser(input: { email: string; name: string }) {
-  const prisma = await getPrisma();
-  return prisma.user.create({ data: input });
-}
