3355715eae62c0e944db5de99054275da81b4be9
Will Madden <madden@prisma.io>
2026-03-28 15:37:14 +0100

    define MongoCommand, MongoWireCommand, and MongoParamRef class hierarchies
    Application-level commands (FindCommand, InsertOneCommand, etc.) carry
    MongoParamRef nodes with values. Wire-level commands (FindWireCommand, etc.)
    carry concrete Document values. The adapter lowering step bridges the two.

--- packages/3-mongo/1-core/src/commands.ts
+++ packages/3-mongo/1-core/src/commands.ts
@@ -1,13 +1,80 @@
+import type { MongoExpr, MongoValue } from './param-ref';
+
 export abstract class MongoCommand {
   readonly collection: string;
 
-  constructor(collection: string) {
+  protected constructor(collection: string) {
     this.collection = collection;
+  }
+
+  protected freeze(): void {
+    Object.freeze(this);
   }
 }
 
-export class FindCommand extends MongoCommand {}
-export class InsertOneCommand extends MongoCommand {}
-export class UpdateOneCommand extends MongoCommand {}
-export class DeleteOneCommand extends MongoCommand {}
-export class AggregateCommand extends MongoCommand {}
+export interface FindOptions {
+  readonly projection?: Record<string, 1 | 0>;
+  readonly sort?: Record<string, 1 | -1>;
+  readonly limit?: number;
+  readonly skip?: number;
+}
+
+export class FindCommand extends MongoCommand {
+  readonly filter: MongoExpr | undefined;
+  readonly projection: Record<string, 1 | 0> | undefined;
+  readonly sort: Record<string, 1 | -1> | undefined;
+  readonly limit: number | undefined;
+  readonly skip: number | undefined;
+
+  constructor(collection: string, filter?: MongoExpr, options?: FindOptions) {
+    super(collection);
+    this.filter = filter;
+    this.projection = options?.projection;
+    this.sort = options?.sort;
+    this.limit = options?.limit;
+    this.skip = options?.skip;
+    this.freeze();
+  }
+}
+
+export class InsertOneCommand extends MongoCommand {
+  readonly document: Record<string, MongoValue>;
+
+  constructor(collection: string, document: Record<string, MongoValue>) {
+    super(collection);
+    this.document = document;
+    this.freeze();
+  }
+}
+
+export class UpdateOneCommand extends MongoCommand {
+  readonly filter: MongoExpr;
+  readonly update: Record<string, MongoValue>;
+
+  constructor(collection: string, filter: MongoExpr, update: Record<string, MongoValue>) {
+    super(collection);
+    this.filter = filter;
+    this.update = update;
+    this.freeze();
+  }
+}
+
+export class DeleteOneCommand extends MongoCommand {
+  readonly filter: MongoExpr;
+
+  constructor(collection: string, filter: MongoExpr) {
+    super(collection);
+    this.filter = filter;
+    this.freeze();
+  }
+}
+
+export class AggregateCommand extends MongoCommand {
+  readonly pipeline: ReadonlyArray<Record<string, unknown>>;
+
+  constructor(collection: string, pipeline: ReadonlyArray<Record<string, unknown>>) {
+    super(collection);
+    this.pipeline = pipeline;
+    this.freeze();
+  }
+}
--- packages/3-mongo/1-core/src/param-ref.ts
+++ packages/3-mongo/1-core/src/param-ref.ts
@@ -17,4 +17,8 @@ export class MongoParamRef {
     this.codecId = options?.codecId;
     Object.freeze(this);
   }
+
+  static of(value: unknown, options?: { name?: string; codecId?: string }): MongoParamRef {
+    return new MongoParamRef(value, options);
+  }
 }
