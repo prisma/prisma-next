d35d396ac614d5cfb5a0d9a42f2568d0d7cf596d
Will Madden <madden@prisma.io>
2026-04-02 20:33:51 +0200

    move framework-components and framework-authoring types to new package
    Copy framework-components.ts and framework-authoring.ts from
    @prisma-next/contract into @prisma-next/framework-components,
    updating the types import to use @prisma-next/contract/types.
    Create barrel exports for ./components and ./authoring subpaths.

---
+++ packages/1-framework/1-core/shared/framework-components/src/exports/authoring.ts
@@ -0,0 +1,22 @@
+export type {
+  AuthoringArgRef,
+  AuthoringArgumentDescriptor,
+  AuthoringColumnDefaultTemplate,
+  AuthoringContributions,
+  AuthoringFieldNamespace,
+  AuthoringFieldPresetDescriptor,
+  AuthoringFieldPresetOutput,
+  AuthoringStorageTypeTemplate,
+  AuthoringTemplateValue,
+  AuthoringTypeConstructorDescriptor,
+  AuthoringTypeNamespace,
+} from '../framework-authoring';
+export {
+  instantiateAuthoringFieldPreset,
+  instantiateAuthoringTypeConstructor,
+  isAuthoringArgRef,
+  isAuthoringFieldPresetDescriptor,
+  isAuthoringTypeConstructorDescriptor,
+  resolveAuthoringTemplateValue,
+  validateAuthoringHelperArguments,
+} from '../framework-authoring';
