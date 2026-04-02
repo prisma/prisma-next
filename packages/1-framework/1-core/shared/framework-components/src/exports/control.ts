7e3b12e944292999ee32a5556f44f22fb5eb42c1
Will Madden <madden@prisma.io>
2026-04-02 20:38:36 +0200

    move assembly logic to control-stack.ts with new names
    Rename assembleComponents -> createControlStack,
    AssembledComponentState -> ControlStack,
    AssembleComponentsInput -> CreateControlStackInput.
    Update imports to use @prisma-next/contract/types for TypesImportSpec.

---
+++ packages/1-framework/1-core/shared/framework-components/src/exports/control.ts
@@ -0,0 +1,18 @@
+export type {
+  AssembledAuthoringContributions,
+  AssemblyInput,
+  ControlStack,
+  CreateControlStackInput,
+} from '../control-stack';
+
+export {
+  assembleAuthoringContributions,
+  assertUniqueCodecOwner,
+  createControlStack,
+  extractCodecTypeImports,
+  extractComponentIds,
+  extractOperationTypeImports,
+  extractParameterizedRenderers,
+  extractParameterizedTypeImports,
+  extractQueryOperationTypeImports,
+} from '../control-stack';
