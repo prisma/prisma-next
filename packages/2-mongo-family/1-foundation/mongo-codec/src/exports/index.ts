fa36859a8b63f510bd4c15f13891412758fbb263
Will Madden <madden@prisma.io>
2026-04-05 20:48:19 +0200

    refactor: extract @prisma-next/mongo-codec from mongo-core
    Move MongoCodec interface, mongoCodec() factory, MongoCodecRegistry,
    and related types into a dedicated foundation package. Update adapter
    codecs import to use the new package.

---
+++ packages/2-mongo-family/1-foundation/mongo-codec/src/exports/index.ts
@@ -0,0 +1,4 @@
+export type { MongoCodecRegistry } from '../codec-registry';
+export { createMongoCodecRegistry } from '../codec-registry';
+export type { MongoCodec, MongoCodecJsType, MongoCodecTrait, MongoCodecTraits } from '../codecs';
+export { mongoCodec } from '../codecs';
