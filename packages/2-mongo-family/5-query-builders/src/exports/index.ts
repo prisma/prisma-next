c8b755ae8f4376c8fb3e58cf51e499aed4983411
Will Madden <madden@prisma.io>
2026-04-06 12:53:39 +0200

    export mongoRaw, MongoRawClient, and RawMongoCollection from @prisma-next/mongo-orm

--- packages/2-mongo-family/5-query-builders/src/exports/index.ts
+++ packages/2-mongo-family/5-query-builders/src/exports/index.ts
@@ -4,6 +4,9 @@ export { compileMongoQuery } from '../compile';
 export type { MongoQueryExecutor } from '../executor';
 export type { MongoOrmClient, MongoOrmOptions } from '../mongo-orm';
 export { mongoOrm } from '../mongo-orm';
+export type { MongoRawClient } from '../mongo-raw';
+export { mongoRaw } from '../mongo-raw';
+export type { RawMongoCollection } from '../raw-collection';
 export type {
   CreateInput,
   DefaultModelRow,
