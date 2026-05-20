3cdd0caeb1cd6fb268bfcaa1d20c727d595a76ba
Will Madden <madden@prisma.io>
2026-05-20 09:55:37 +0200

    feat(@prisma-next/mongo): add /control subpath
    Signed-off-by: Will Madden <madden@prisma.io>

--- packages/3-extensions/mongo/tsdown.config.ts
+++ packages/3-extensions/mongo/tsdown.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from '@prisma-next/tsdown';
 
 export default defineConfig({
   entry: [
+    'src/exports/bson.ts',
     'src/exports/config.ts',
     'src/exports/contract-builder.ts',
+    'src/exports/control.ts',
     'src/exports/family.ts',
-    'src/exports/index.ts',
     'src/exports/runtime.ts',
     'src/exports/target.ts',
   ],
